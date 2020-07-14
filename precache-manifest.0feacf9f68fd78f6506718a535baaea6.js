1e57ae22cf8ad23a6ad07bee024581fc10c57648
odednnechmad <odednnechmad@gmail.com>
2020-07-14 10:34:43 +0300

    Updates

---
+++ precache-manifest.0feacf9f68fd78f6506718a535baaea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfaf65c56e978dee2e63aa6a38f1f40",
+    "url": "/Smart/index.html"
+  },
+  {
+    "revision": "73791ab6c8e9ce7eee8d",
+    "url": "/Smart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c3ed45e3920e75ac6a5c",
+    "url": "/Smart/static/css/main.4ad465da.chunk.css"
+  },
+  {
+    "revision": "73791ab6c8e9ce7eee8d",
+    "url": "/Smart/static/js/2.a0c9c70f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Smart/static/js/2.a0c9c70f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ed45e3920e75ac6a5c",
+    "url": "/Smart/static/js/main.30c6ed81.chunk.js"
+  },
+  {
+    "revision": "1a05332ec4aaca80f31c",
+    "url": "/Smart/static/js/runtime-main.388c4a58.js"
+  }
+]);
